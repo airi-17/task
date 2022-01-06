abbcc3c2a3211d6bf65155d106890e12dc99918b
airi-17 <79825125+airi-17@users.noreply.github.com>
2022-01-06 21:38:25 +0900

    Add files via upload

---
+++ jstask/task-javascript.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  const btn = document.getElementById('btn');
+  const techgeekheader = document.getElementById('techgeek-header');
+  btn.addEventListener('click', function() {
+    techgeekheader.innerHTML = '<h1>テックギーク</h1>';
+  })
+})
